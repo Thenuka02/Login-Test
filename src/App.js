2482f7c9d5195f2cae92490ef6d0c71b7fdb1ff9
Thenuka02 <kthenuka02@gmail.com>
2021-10-06 10:14:41 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@
 import React from 'react';
 
 import SignInPage from './components/SignIn/SignInPage';
-import hcbgImage from './components/image/bg2.jpg'
+//import hcbgImage from './image/bg2.jpg';
 
 
 function App() {
   return (
-    
     <div
-    class="bg_image"
-    style={{
-      backgroundImage: `url(${hcbgImage})`,
-      backgroundSize: "cover",
-      height: "100vh",
-      width: "150vh",
-      color: "#f5f5f5"
-    }}
+    
   >
 
       <SignInPage />
