b8bbf95909b88a894590c0063fb8e3cff1d6062b
Thenuka02 <kthenuka02@gmail.com>
2021-10-12 07:46:06 +0530

    Changes

--- src/components/SignIn/SignInPage.js
+++ src/components/SignIn/SignInPage.js
@@ -1,30 +1,57 @@
 import React from 'react';
 
-import Card from '../UI/Card';
-import  classes from './SignInPage.module.css';
-import Button from '../UI/Button';
-import Logo from '../../components/image/signup.png'
+
+//import Card from '../UI/Card';
+//import  classes from './SignInPage.module.css';
+//import Button from '../UI/Button';
+import Logo from '../../image/loginsam1.jpg'
+//import './login.css';
+//import {ReactComponent as Logo} from '../../image/signup.png'
+//import { ReactComponent as Logo } from '../../image/signup.png';
+import hcbgImage from '../../image/bg2.jpg';
+
+import './loginPage.css';
 
 const SignInPage = props => {
     const addUserHandler = (event) =>{
         event.preventDefault();
     };
+ 
     return (
-       <Card >
-        <form onSubmit={addUserHandler}>
-            <div className='div-login-Logo'> 
-            <img src={Logo} alt='Logo'></img> </div>
-            <h1> Sign In</h1>
-            <input className={classes.input} type="email" name="email"  placeholder="example@gmail.com" /><br/>
-            <input className={classes.input} type="password" name="password" placeholder="Enter Password" /> <br/>
-            <div class="checkboxy">
-                <input name="cecky" id="checky" value="1" type="checkbox" /><label class="terms">Remember me</label>
+        <div class='main-image'>
+            <img src={hcbgImage} alt='nature' />
+            <div className='div-login'> 
+            <div className='div-login-logo'>
+            <img src={Logo} alt='Logo'></img>
+            </div> 
+            
+            <div>
+            <form onSubmit={addUserHandler}>
+            <h1 > Sign In</h1>
+            <input  type="email" name="email"  placeholder="Enter Email" /><br/>
+            <input type="password" name="password" placeholder="Enter Password" /> <br/>
+            
+            <button type="submit" text-align="center">SIGN IN</button>
+            <div className='link'>
+            <a  href="#">Forgot password?</a> 
             </div>
-            <Button type="submit">SIGN IN</Button>
+            <div className="link2">
+            <a  href="#">Don't have an account? </a>
+            </div>
+           
+            <div className="link3">Copyright ©
+            <a  href="#"> My website </a> 2021
+            </div>
+            
+            
         </form>
-       </Card>
-    );
-
-};
+        </div>
+        </div>
+        </div>
+    
+        
+        
+    )
+}
 
 export default SignInPage;
