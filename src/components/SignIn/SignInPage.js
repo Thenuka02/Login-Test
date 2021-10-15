0c9137cd594e271047cf92ac33ae0d170e04c40f
Thenuka02 <kthenuka02@gmail.com>
2021-10-15 07:23:22 +0530

    changes in footer class

--- src/components/SignIn/SignInPage.js
+++ src/components/SignIn/SignInPage.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 
+
 //import Card from '../UI/Card';
 //import  classes from './SignInPage.module.css';
 //import Button from '../UI/Button';
 //import Logo from '../../image/loginsam1.jpg'
 //import './login.css';
 import {ReactComponent as Logo} from '../../image/icons8-contacts.svg'
-//import { ReactComponent as Logo } from '../../image/signup.png';
 import hcbgImage from '../../image/bg2.jpg';
+import {ReactComponent as FB} from '../../image/icons8-facebook.svg'
+import {ReactComponent as INSTA} from '../../image/icons8-instagram.svg'
+import {ReactComponent as LINKEDIN} from '../../image/icons8-linkedin.svg'
+
 
 import './loginPage.css';
 
@@ -18,20 +22,21 @@ const SignInPage = props => {
     };
  
     return (
-        <div class='main-image'>
+       <div class="wrapper">
+        <div className='main-image'>
             <img src={hcbgImage} alt='nature' />
+         </div> 
+           
+            <div>
             <div className='div-login'>
-                <div className='div-login-logo'> 
-            {/* <div className='div-login-logo'>
-            <img src={Logo} alt='Logo'></img>
-            </div> 
-             */}
-            
+                <div className='logo'> 
                  <Logo />
              </div>
-            <div>
+            <h1 className='h1'> Sign In</h1>
+           
             <form onSubmit={addUserHandler}>
-            <h1 > Sign In</h1>
+               
+            
             <input  type="email" name="email"  placeholder="Enter Email" /><br/>
             <input type="password" name="password" placeholder="Enter Password" /> <br/>
             
@@ -42,18 +47,21 @@ const SignInPage = props => {
             <div className="link2">
             <a  href="#">Don't have an account? </a>
             </div>
-           
             <div className="link3">Copyright ©
             <a  href="#"> My website </a> 2021
             </div>
-            
+            <div className='footer' >
+                < FB /> 
+                < INSTA />
+                < LINKEDIN /> 
+            </div>  
             
         </form>
         </div>
-        </div>
-        </div>
+       </div>
+       </div>
     
-        
+     
         
     )
 }
